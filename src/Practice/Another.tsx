7d095498d5b46ed9ef24c46a2e265168701fbb2e
yeeahG <yeji.sep.k@gmail.com>
2023-01-04 23:25:51 +0900

    Drag and drop 기능 추가

---
+++ src/Practice/Another.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { minuteState } from "../atoms";
+import { hourSelector } from "./AnoAtoms";
+
+function Another () {
+    const [minutes, setMinutes] = useRecoilState(minuteState);
+    const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setMinutes(+event.currentTarget.value);
+    }
+
+    const [hours, setHours] = useRecoilState(hourSelector);
+    const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value);
+    }
+
+    return (
+        <div>
+            <input 
+                value={minutes} 
+                onChange={onMinutesChange}
+                type="number" 
+                placeholder="Minutes" 
+            />
+            <input 
+                onChange={onHoursChange}
+                value={hours} type="number" 
+                placeholder="Hours" 
+            />
+        </div>
+    )
+}
+
+export default Another;
