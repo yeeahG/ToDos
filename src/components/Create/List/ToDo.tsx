253bf09d1fc81068da0b76772e24270116aaec38
yeeahG <yeji.sep.k@gmail.com>
2023-01-04 19:53:44 +0900

    category 완료 기능

--- src/components/Create/List/ToDo.tsx
+++ src/components/Create/List/ToDo.tsx
@@ -1,18 +1,41 @@
+import React from "react";
 import { IToDo } from "../../atoms";
 
-function ToDo( {text, category }:IToDo ) {
+function ToDo( {text, category, id }:IToDo ) {
     //To do category 변경
+    /*
     const onClick = (newCategory: IToDo["category"]) => {
         console.log("I wanna ", newCategory);
-        
+    }*/
+    const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+        const {currentTarget:{name}} = event;
     }
 
     return (
     <li>
         <span>{text}</span>
+        {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+         )}
+        {category !== "TODO" && (
+            <button name="TODO" onClick={onClick}>
+            To Do
+            </button>
+        )}
+        {category !== "DONE" && (
+            <button name="DONE" onClick={onClick}>
+            Done
+            </button>
+        )}
+
+
+        {/*
         {category !== "TODO" && <button onClick={() => onClick("TODO")}>To Do</button>}
         {category !== "DOING" && <button onClick={() => onClick("DOING")}>-ing</button>}
         {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>}
+        */}
     </li>
     );
 }
