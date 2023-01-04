2843229bc2c5bdf6249b7c6a9f0c09bdd9da2364
yeeahG <yeji.sep.k@gmail.com>
2023-01-04 22:24:41 +0900

    Recoil, localstage 이용하여 데이터 저장

--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -13,10 +13,27 @@ export interface IToDo {
     id: number;
 }
 
+const localStorageEffect = (key: string) => ({setSelf, onSet}: any) => {
+    const savedValue = localStorage.getItem(key)
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+  
+    onSet((newValue: any , _: any, isReset: boolean) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+};
+
 export const toDoState = atom<IToDo[]>({
     key:"toDo",
     default: [],
+    effects: [
+        localStorageEffect('toDo'),
+    ]
 })
+
 
 export const toDoSelector = selector({
     key: "toDoSelector",
